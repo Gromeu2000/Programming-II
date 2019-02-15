739d1eca6574acf7272bdc9530619ec686d67756
Gromeu2000 <45124641+Gromeu2000@users.noreply.github.com>
2019-02-15 10:27:40 +0100

    Char pointers

--- Pointers/Pointers/Source.cpp
+++ Pointers/Pointers/Source.cpp
@@ -38,7 +38,7 @@ int main() {
 }
 
 #endif
-
+#if 0
 int main() {
 
 	int vec[3] = { 1,2,3 };
@@ -48,7 +48,75 @@ int main() {
 
 		std::cout << vec[i] << std::endl;
 	}
-	
+
+	for (int i = 0; i < 3; i++) {
+
+		std::cout << *(vec + i) << std::endl;
+	}
+
+	for (int i = 0; i < 3; i++) {
+
+		std::cout << *(ptr2a++) << std::endl;
+	}
 	system("pause");
 	return 0;
 }
+#endif
+#if 0
+void add5(int *p) {
+
+	*p += 5; // tambien se puede poner *p = *p + 5; 
+}
+
+int main() {
+
+	int num = 5;
+	add5(&num);
+	std::cout << num << std::endl;
+
+	system("pause");
+	return 0;
+}
+#endif
+#if 0
+int sum(int *ptr, int size) {
+
+	int result = 0;
+	
+	for (int i = 0; i < size; i++) {
+
+		result += ptr[i];
+	}
+
+	return result;
+}
+
+int main() {
+	
+	int array[3] = { 1,2,3 };
+	int res = sum(array, 3);
+
+	std::cout << res << std::endl;
+
+	system("pause");
+	return 0;
+}
+#endif
+#if 0
+void charFunc(const char *ptr, int size) {
+
+	for (int i = 0; ptr[i] != '\0'; i++) {
+
+	std::cout << ptr[i] << std::endl;
+	}
+
+}
+
+int main() {
+	const char *ptr = "Hello world of files";
+	charFunc(ptr, 90);
+
+	system("pause");
+	return 0;
+}
+#endif
