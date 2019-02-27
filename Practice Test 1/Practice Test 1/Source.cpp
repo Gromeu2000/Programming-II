844f925cdfc884e2d1aea0bf473be305db347baa
Gromeu2000 <45124641+Gromeu2000@users.noreply.github.com>
2019-02-27 14:16:47 +0100

    S

--- Practice Test 1/Practice Test 1/Source.cpp
+++ Practice Test 1/Practice Test 1/Source.cpp
@@ -2,5 +2,17 @@
 
 int main() {
 
+	/*int array[10] = {};
+	int *ptr = array;
+	int *ptra = ptr;
 
+	for (int i = 0; i < 10; i++) {
+
+		ptra[i] = &array[i];
+		std::cout << *(ptra + i) << std::endl;
+
+	}*/
+
+	system("pause");
+	return 0;
 }
