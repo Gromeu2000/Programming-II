f614bc7320f1aaa1e4e9819ca629e44fbe4cab44
Gromeu2000 <45124641+Gromeu2000@users.noreply.github.com>
2019-03-20 23:50:00 +0100

    Friendship

---
+++ Friendship Exercises/Friendship Exercises/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>;
+
+class Player {
+
+private:
+	const char *name;
+	unsigned short life;
+	bool invincible = false;
+	bool* capabilities = new bool[3]();
+	bool* weapon = new bool[3]();
+
+public:
+	
+};
+
+int main() {
+
+
+}
