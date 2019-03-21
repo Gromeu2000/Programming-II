16410d8aa3484e48ef554aa01d57b3017d5b26b4
Gromeu2000 <45124641+Gromeu2000@users.noreply.github.com>
2019-03-21 23:06:54 +0100

    Finish in class

--- Friendship Exercises/Friendship Exercises/Source.cpp
+++ Friendship Exercises/Friendship Exercises/Source.cpp
@@ -1,9 +1,12 @@
-#include <iostream>;
+#include <iostream>
+#include <string.h>
+
+typedef const char cchar;
 
 class Player {
 
 private:
-	const char *name;
+	cchar *name;
 	unsigned short life;
 	bool invincible = false;
 	bool* capabilities = new bool[3]();
@@ -11,9 +14,92 @@ private:
 
 public:
 	
+	Player() : name("Pepe"), life(3), invincible(false), capabilities(false), weapon(false) {}
+
+	~Player() {}
+
+	unsigned char getName() const {
+
+		std::cout << name << std::endl;
+		
+		return *name;
+		
+	}
+
+	void PrintNumLifes() const {
+
+		std::cout << life << std::endl;
+	}
+
+	void PrintInvincible() const {
+
+		while (invincible) {
+
+			std::cout << "The player is invincible" << std::endl;
+			return;
+		}
+
+		std::cout << "The player is not invincible" << std::endl;
+	}
+
+	void PrintCapibilities() const {
+
+		if (capabilities) {
+
+			for (int i = 0; i < 3; i++) {
+
+				std::cout << capabilities[i] << std::endl;
+			}
+			
+		}
+	}
+
+	void PrintWeapon() const {
+
+		if (weapon) {
+
+			for (int i = 0; i < 3; i++) {
+
+				std::cout << weapon[i] << std::endl;
+			}
+
+		}
+	}
+
+	void EqualOperator() const {
+
+		const char* name2 = "Pepe";
+
+		if (strcmp(name, name2) == 0) {
+
+			std::cout << "Sorry, same player" << std::endl;
+		}
+
+		else {
+
+			std::cout << "Available name" << std::endl;
+		}
+	}
+};
+
+class Item{
+
+private:
+
+	cchar *one_up = "One_Up";
+	cchar *mini_mario = "Mini_Mario";
+	cchar *super_mario = "Super_Mario";
+	cchar *fire_flower = "Fire-Flower";
+	cchar *ice_flower = "Ice_Flower";
+	cchar *golden_flower = "Golden_Flower";
+
+public:
+	Item(cchar *ou, cchar *mm, cchar *sp, cchar *ff, cchar *iF, cchar *gf) : one_up(ou), mini_mario(mm), super_mario(sp), fire_flower(ff), ice_flower(iF), golden_flower(gf){}
+	~Item() {}
 };
 
 int main() {
 
-
+	system("pause");
+	return 0;
 }
