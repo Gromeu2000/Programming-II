16457493f56774473c63c7b9585f7d898690889d
Gromeu2000 <45124641+Gromeu2000@users.noreply.github.com>
2019-05-13 23:30:35 +0200

    Recursive done

--- Recursivity Exercises/Project1/Source.cpp
+++ Recursivity Exercises/Project1/Source.cpp
@@ -76,17 +76,34 @@ bool searchRec(int vec[], int sizeVec, unsigned int index, int val) {
 
 		searchRec(vec, sizeVec, index + 1, val);
 		if (vec[index] == val) {
-
-			std::cout << "The val is in the array" << std::endl;
-			return true;
+			
+			return true + searchRec(vec, sizeVec, index + 1, val);
+			
 		}
 		else {
 
-			std::cout << "The val is not in the array" << std::endl;
+			return false + searchRec(vec, sizeVec, index + 1, val);
+			
 		}
 	}
 	else {
 		return false;
+	}
+}
+
+int sumElemsRec(int vec[], unsigned int sizeVec, unsigned int index) {
+
+	if (index < sizeVec) {
+
+		int sum = 0;
+		sumElemsRec(vec, sizeVec, index + 1);
+		sum += vec[index];
+		return sum + sumElemsRec(vec, sizeVec, index + 1);
+
+	}
+	else {
+
+		return 0;
 	}
 }
 int main() {
@@ -96,8 +113,19 @@ int main() {
 	std::cout << recursiveFunc3(9) << std::endl;
 	bool vec[] = { true, false, true, true, true, false };
 	int vec1[] = { 1, 2, 3, 4, 5, 6, 7 };
+	int vec2[] = { 11, 5, 6, 7, 8, 2 };
 	std::cout << numTrues(vec, 6, 0) << std::endl;
-	std::cout << searchRec(vec1, 7, 0, 4) << std::endl;
+
+	if (searchRec(vec1, 7, 0, 7) == true) {
+
+		std::cout << "The number is in the array" << std::endl;
+	}
+	else if (searchRec(vec1, 7, 0, 7) == false) {
+
+		std::cout << "The number is not in the array"<< std::endl;
+	}
+
+	std::cout << sumElemsRec(vec2, 6, 0) << std::endl;
 	system("pause");
 	return 0;
 }
